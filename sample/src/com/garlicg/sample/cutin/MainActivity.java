b39ae5306040249d6a4563aa42f33572b2e69631
garlicG <ggarlicG@gmail.com>
2013-09-04 05:20:08 +0900

    changed to card ui

--- sample/src/com/garlicg/sample/cutin/MainActivity.java
+++ sample/src/com/garlicg/sample/cutin/MainActivity.java
@@ -2,56 +2,68 @@ package com.garlicg.sample.cutin;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
+import com.garlicg.cutinlib.CutinItem;
 import com.garlicg.cutinlib.SimpleCutinScreen;
-import com.garlicg.cutinlib.SimpleCutinScreen.CutinItem;
-import com.garlicg.cutinlib.SimpleCutinScreen.PickListener;
 
-	public class MainActivity extends ActionBarActivity {
+	public class MainActivity extends ActionBarActivity implements SimpleCutinScreen.PickListener {
 		
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		// SimpleCutinScreen read the intent for switching state PICK or VIEW.
-		SimpleCutinScreen screen = new SimpleCutinScreen(this , getIntent());
-		setContentView(screen.getView());
-		
-		// create CUT-IN list.
-		ArrayList<CutinItem> items = new ArrayList<CutinItem>();
-		items.add(new CutinItem(GarlinTornado.class,getString(R.string.garlin_tornado)));
-		items.add(new CutinItem(GarlinMirage.class, getString(R.string.garlin_mirage)));
-		items.add(new CutinItem(GarlinParade.class, getString(R.string.garlin_parade)));
-		screen.setCutinList(items , getIntent());
-		
-		// when call from official cut-in app , screen layout state is STATE_PICK , other STATE_VIEW.
-		if(screen.getState() == SimpleCutinScreen.STATE_PICK){
-			screen.setListener(new PickListener() {
-				@Override
-				public void ok(Intent intent) {
-					setResult(Activity.RESULT_OK, intent);
-					finish();
-				}
-				@Override
-				public void cancel() {
-					finish();
-				}
-			});
-			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		private SimpleCutinScreen mScreen;
+		@Override
+		protected void onResume() {
+			super.onResume();
+			mScreen.resume();
 		}
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getItemId() == android.R.id.home){
+		
+		@Override
+		protected void onPause() {
+			super.onPause();
+			mScreen.pause();
+		}
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+
+			mScreen = new SimpleCutinScreen(this , getIntent());
+			setContentView(mScreen.getView());
+			
+			// create cutin list
+			ArrayList<CutinItem> items = new ArrayList<CutinItem>();
+			items.add(new CutinItem(GarlinTornado.class,getString(R.string.garlin_tornado)));
+			items.add(new CutinItem(GarlinMirage.class, getString(R.string.garlin_mirage)));
+			items.add(new CutinItem(GarlinParade.class, getString(R.string.garlin_parade)));
+			mScreen.setCutinList(items);
+			
+			// pick mode
+			if(mScreen.getState() == SimpleCutinScreen.STATE_PICK){
+				mScreen.setListener(this);
+				getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+			}
+		}
+		
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			if(item.getItemId() == android.R.id.home){
+				finish();
+				return true;
+			}
+			return super.onOptionsItemSelected(item);
+		}
+
+		@Override
+		public void ok(Intent intent) {
+			setResult(RESULT_OK, intent);
 			finish();
-			return true;
 		}
-		return super.onOptionsItemSelected(item);
-	}
+
+		@Override
+		public void cancel() {
+			setResult(RESULT_CANCELED);
+			finish();
+		}
 }
