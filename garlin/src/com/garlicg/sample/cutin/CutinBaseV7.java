7c94f49ee4f010bb1272916ea1d520a4adb3a9cd
garlicG <ggarlicG@gmail.com>
2013-09-07 00:49:57 +0900

    added CutinBasev7

---
+++ garlin/src/com/garlicg/sample/cutin/CutinBaseV7.java
@@ -0,0 +1,63 @@
+package com.garlicg.sample.cutin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+
+import com.garlicg.cutinlib.SimpleCutinScreen;
+
+public class CutinBaseV7 extends ActionBarActivity implements SimpleCutinScreen.PickListener{
+	private SimpleCutinScreen mScreen;
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mScreen.resume();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mScreen.pause();
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mScreen = new SimpleCutinScreen(this , getIntent());
+		setContentView(mScreen.getView());
+		
+		if(mScreen.getState() == SimpleCutinScreen.STATE_PICK){
+			mScreen.setListener(this);
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+	
+	protected SimpleCutinScreen getCutinScreen(){
+		return mScreen;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == android.R.id.home){
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void ok(Intent intent) {
+		setResult(RESULT_OK, intent);
+		finish();
+	}
+
+	@Override
+	public void cancel() {
+		setResult(RESULT_CANCELED);
+		finish();
+	}
+
+}
--- garlin/src/com/garlicg/sample/cutin/MainActivity.java
+++ garlin/src/com/garlicg/sample/cutin/MainActivity.java
@@ -2,68 +2,21 @@ package com.garlicg.sample.cutin;
 
 import java.util.ArrayList;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.MenuItem;
 
 import com.garlicg.cutinlib.CutinItem;
-import com.garlicg.cutinlib.SimpleCutinScreen;
 
-	public class MainActivity extends ActionBarActivity implements SimpleCutinScreen.PickListener {
-		
-		private SimpleCutinScreen mScreen;
-		@Override
-		protected void onResume() {
-			super.onResume();
-			mScreen.resume();
-		}
-		
-		@Override
-		protected void onPause() {
-			super.onPause();
-			mScreen.pause();
-		}
+	public class MainActivity extends CutinBaseV7 {
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-
-			mScreen = new SimpleCutinScreen(this , getIntent());
-			setContentView(mScreen.getView());
 			
 			// create cutin list
 			ArrayList<CutinItem> items = new ArrayList<CutinItem>();
 			items.add(new CutinItem(GarlinTornado.class,getString(R.string.garlin_tornado)));
 			items.add(new CutinItem(GarlinMirage.class, getString(R.string.garlin_mirage)));
 			items.add(new CutinItem(GarlinParade.class, getString(R.string.garlin_parade)));
-			mScreen.setCutinList(items);
-			
-			// pick mode
-			if(mScreen.getState() == SimpleCutinScreen.STATE_PICK){
-				mScreen.setListener(this);
-				getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-			}
-		}
-		
-		@Override
-		public boolean onOptionsItemSelected(MenuItem item) {
-			if(item.getItemId() == android.R.id.home){
-				finish();
-				return true;
-			}
-			return super.onOptionsItemSelected(item);
-		}
-
-		@Override
-		public void ok(Intent intent) {
-			setResult(RESULT_OK, intent);
-			finish();
-		}
-
-		@Override
-		public void cancel() {
-			setResult(RESULT_CANCELED);
-			finish();
+			getCutinScreen().setCutinList(items);
 		}
 }
