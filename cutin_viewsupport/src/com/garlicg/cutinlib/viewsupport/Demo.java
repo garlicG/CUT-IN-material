f6c784abe7a8e0a4898bb92f75176801a2b87cee
garlicG <ggarlicG@gmail.com>
2013-09-23 03:45:03 +0900

    Divided lib of view.

---
+++ cutin_viewsupport/src/com/garlicg/cutinlib/viewsupport/Demo.java
@@ -0,0 +1,34 @@
+package com.garlicg.cutinlib.viewsupport;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+import com.garlicg.cutinlib.CutinInfo;
+import com.garlicg.cutinlib.CutinService;
+
+public class Demo {
+	private Intent mCutinIntent;
+	private Context mContext;
+	public Demo(Context context){
+		mContext = context;
+	}
+
+	public ComponentName play(Class<? extends CutinService> serviceClass , int cutinId) {
+		if (mCutinIntent != null) {
+//			if(!mCutinIntent.getComponent().getClassName().equals(serviceClass.getName())){
+			mContext.stopService(mCutinIntent);
+//			}
+		}
+		mCutinIntent = new Intent(mContext, serviceClass);
+		mCutinIntent.putExtra(CutinInfo.DATA_CUTIN_ID, cutinId);
+		return mContext.startService(mCutinIntent);
+	}
+	
+	public void forceStop(){
+		if(mCutinIntent != null){
+			mContext.stopService(mCutinIntent);
+		}
+		mCutinIntent = null;
+	}
+}
