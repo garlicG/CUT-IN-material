76bdb1d80238b427eeec60872e153eedd464da59
garlicG <ggarlicG@gmail.com>
2013-10-14 11:03:56 +0900

    Added AnimationSetCutin.

--- sample/src/cutin/sample/animation/AnimationSetCutin.java
+++ sample/src/cutin/sample/animation/AnimationSetCutin.java
@@ -1,14 +1,18 @@
 package cutin.sample.animation;
 
-import com.garlicg.cutinlib.CutinService;
-
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.view.animation.Animation.AnimationListener;
-import android.widget.TextView;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
+import com.garlicg.cutinlib.CutinService;
+
 import cutin.sample.R;
 
 public class AnimationSetCutin extends CutinService{
@@ -19,8 +23,6 @@ private View mView;
 		LayoutInflater inflater = LayoutInflater.from(this);
 		View layout = inflater.inflate(R.layout.cutin_default, null);
 		mView = layout.findViewById(R.id.cutin_default_view);
-		TextView textView = (TextView)layout.findViewById(R.id.cutin_default_text);
-		textView.setText("SimpleCutIn");
 		return layout;
 	}
 
@@ -32,9 +34,14 @@ private View mView;
 	protected void start(Intent intent, int flags, int startId) {
 		int centerX = mView.getWidth()/2;
 		int centerY = mView.getHeight()/2;
-		RotateAnimation anim = new RotateAnimation(0.f, 720f , centerX , centerY);
-		anim.setDuration(1500);
-		anim.setAnimationListener(new AnimationListener() {
+		
+		AnimationSet as = new AnimationSet(false);
+		as.addAnimation(new TranslateAnimation(0f, 100f , 0f,100f));
+		as.addAnimation(new AlphaAnimation(0.2f, 1f));
+		as.addAnimation(new RotateAnimation(0f, 720f , centerX , centerY));
+		as.addAnimation(new ScaleAnimation(0.2f, 1f, 0.2f, 1f , centerX , centerY));
+		as.setDuration(2000);
+		as.setAnimationListener(new AnimationListener() {
 			@Override
 			public void onAnimationStart(Animation animation) {}
 			@Override
@@ -45,7 +52,7 @@ private View mView;
 			}
 		});
 		
-		mView.startAnimation(anim);
+		mView.startAnimation(as);
 	}
 }
 
